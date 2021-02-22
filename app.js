1d60a4b6a63b2e333e2387a8211e764ffd749460
bs <bryan@bryansilvia.com>
2021-02-21 20:38:15 -0800

    Getting REST request setup and ready

--- app.js
+++ app.js
@@ -1,42 +1,42 @@
-// $(document).ready(function() {
-//     alert("something");
-//     return
-//     $.ajax({
-//         url: "https://newsapi.org/v2/top-headlines?country=us&amp;apiKey=e03753c9126b408d870a44318813ac3d"
-//     }).then(function(data) {
-
-//         for (i = 0; i < data.articles.length; i++) {
-//           $('#news').append("<ul><li>"+data.articles[i].title+"</li></ul>");
-//         }
-
-//     });
-// });
-
-
 $(document).ready(function () {
     //alert("Handler for .submit() called.");
 
     $("#formSubmitBtn").click(function (event) {
         var key = $("#keyEntry").val()
         var list = $("#addresses").val()
-        
+
         ValidateFormInput(key, list)
         //alert("Handler for .submit() called.");
         // event.preventDefault();
     });
 });
 
-function ValidateFormInput(key, text) {
+function ValidateFormInput(key, addresses) {
     if (key.trim().length == 0) {
         alert("API key required");
         return false;
-    } else {
+    } else if (addresses.trim().length == 0) {
+        alert("Address text is required");
+        return false;
+    }
+    else {
+        var addressList = addresses.split("\n");
+        addressList.forEach(addr => {
 
+        });
     }
 
     return false;
 }
 
 function submitRest(params) {
+    $.ajax({
+        url: "https://newsapi.org/v2/top-headlines?country=us&amp;apiKey=e03753c9126b408d870a44318813ac3d"
+    }).then(function (data) {
 
+        for (i = 0; i < data.articles.length; i++) {
+            $('#news').append("<ul><li>" + data.articles[i].title + "</li></ul>");
+        }
+
+    });
 }
