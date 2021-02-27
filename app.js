52bb1631d1e091f5a373d5591a80eee6f4b60e32
bsilvia <bryan.silvia1@gmail.com>
2021-02-27 10:43:08 -0800

    Ignore addresses which are blank

--- app.js
+++ app.js
@@ -26,7 +26,9 @@ jQuery(function () {
         if (ValidateFormInput(key, list)) {
             var addressList = list.split("\n");
             addressList.forEach(addr => {
-                Search(addr);
+                if (addr.trim().length > 0) {
+                    Search(addr);
+                }
             });
         }
 
@@ -55,7 +57,15 @@ jQuery(function () {
 
 function AddLineToGrid(id, lat, long, description) {
     // TODO - add hyperlink for issue #4
+<<<<<<< HEAD
     var markup = "<tr><td>" + id + "</td><td>" + lat + "</td><td>" + long + "</td><td>" + description + "</td></tr>";
+=======
+    var markup = "<tr><td>" + id + "</td><td>" + lat + "</td><td>" + long + "</td><td>" +
+        "<a href=\"https://www.bing.com/maps?sp=point." + lat + "_" + long + "_" + encodeURIComponent(description) +
+        "\" target=\"_blank\" rel=\"noopener noreferrer\">" +
+        description + "</a>" +
+        "</td></tr>";
+>>>>>>> ab6c0bb... Ignore addresses which are blank
     $("table tbody").append(markup);
 }
 
